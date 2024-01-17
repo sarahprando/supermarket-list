5e245badedc1a8dff4fdc07da8f73e44bb504501
Sarah Prando <sarahprando40@gmail.com>
2024-01-17 19:22:38 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,4 @@
-function initMenu() {
-    const buttonMenu = document.querySelectorAll('.btn-menu img');
-    const activeClass = 'ativo'
-    
-    if(buttonMenu.length){
-    buttonMenu[0].nextElementSibling.classList.add(activeClass);
-    
-    function activeAccordion(event) {
-        this.classList.toggle(activeClass);
-        this.nextElementSibling.classList.toggle(activeClass);
-    }
-    
-    buttonMenu.forEach((item) => {
-        item.addEventListener('mouseover', activeAccordion);
-    });
-    }
-    }
-    
-
-    initMenu();
+function activeNav() {
+    const ativarMenu = document.querySelector('.navigation nav');
+    ativarMenu.classList.toggle('ativo')
+}
